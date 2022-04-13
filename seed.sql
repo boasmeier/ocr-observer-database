3540a92e103c0f6b8fe95f0b5f41773d8f4794ba
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-04-13 20:20:48 +0200

    feat: add sql script to seed db

---
+++ seed.sql
@@ -0,0 +1,7 @@
+use ocr_observer;
+
+select * from state;
+insert into state (name) values ("Hochgeladen");
+insert into state (name) values ("Erkannt");
+insert into state (name) values ("Bestätigt");
+insert into state (name) values ("Zurückgewiesen");
