52001f9f08f3e6939844ea24a7b8777e117d3a63
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-04-18 13:42:07 +0200

    fix: remove umlaute

--- seed.sql
+++ seed.sql
@@ -2,8 +2,8 @@ use ocr_observer;
 
 insert into state (name) values ("Hochgeladen");
 insert into state (name) values ("Erkannt");
-insert into state (name) values ("Bestätigt");
-insert into state (name) values ("Zurückgewiesen");
+insert into state (name) values ("Bestaetigt");
+insert into state (name) values ("Zurueckgewiesen");
 
 insert into task (name, description) values ("test_task", "This is a test task");
 insert into dataset (idtask, name, description) values (1, "test_dataset", "This is a test dataset");
