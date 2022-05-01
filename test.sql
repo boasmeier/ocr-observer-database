8428306200441afbc4f43bf7ad939576fe08e40d
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-01 21:31:52 +0200

    Add some test queries to test script

--- test.sql
+++ test.sql
@@ -7,3 +7,27 @@ select * from history;
 select * from history_has_state;
 select * from task;
 select * from dataset;
+
+# Get image with fields and state
+select image.idimage, fields.idfields, image.name, image.mimetype, fields.signature, fields.author, fields.title, fields.comment, fields.category, state.name 
+from image join fields join history_has_state join state 
+where image.idfields = fields.idfields
+and image.idhistory = history_has_state.idhistory
+and history_has_state.idstate = state.idstate;
+
+# Get image with state
+SELECT * FROM (
+	SELECT image.idimage, image.iddataset, image.name, image.mimetype, state.name as state, history_has_state.timestamp, row_number() over(partition by image.name order by history_has_state.timestamp desc) as rn
+	FROM image JOIN history_has_state JOIN state 
+	WHERE image.idhistory = history_has_state.idhistory
+	AND history_has_state.idstate = state.idstate ) t
+WHERE t.rn = 1;
+
+# Get image histroy with state
+SELECT history_has_state.idhistory, state.name as state, history_has_state.timestamp
+FROM history_has_state JOIN state JOIN image
+WHERE history_has_state.idstate = state.idstate
+AND history_has_state.idhistory = image.idhistory
+AND image.idimage = 1
+ORDER BY timestamp desc;
+
