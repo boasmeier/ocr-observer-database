fac974a9be08630b8b1f893e42f2b8a4746a9f1a
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-21 16:20:07 +0200

    feat: use umlaute in seed

--- seed.sql
+++ seed.sql
@@ -2,8 +2,8 @@ use ocr_observer;
 
 insert into state (name) values ("Hochgeladen");
 insert into state (name) values ("Erkannt");
-insert into state (name) values ("Bestaetigt");
-insert into state (name) values ("Zurueckgewiesen");
+insert into state (name) values ("Bestätigt");
+insert into state (name) values ("Zurückgewiesen");
 
 insert into task (name, description) values ("Zettelkatalog", "Zettel beinhalten Signatur, Autor, Titel, Bemerkung und ggf. Kategorie.");
 insert into dataset (idtask, name, description) values (1, "Autor Datensatz A-Z", "Dieser Datensatz beinhaltet sämtliche Registerkarten des Autor Datensatz.");
